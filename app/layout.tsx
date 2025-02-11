7f9e8c0dc7e691cc13b27e0af73728f68f39fa97
AmanYadav007 <amanry3000@gmail.com>
2025-02-12 02:40:54 +0530

    upading files

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,9 @@ export const metadata: Metadata = {
   title: 'Image Resizer',
   description: 'Resize images directly from your browser.',
   icons: {
-    icon: '/favicon.ico', // Path to your favicon in the public folder
-    shortcut: '/favicon.ico', // Optional: Shortcut icon
-    apple: '/apple-touch-icon.png', // Optional: Apple touch icon
+    icon: 'favicon.ico', // Path to your favicon in the public folder
+    shortcut: 'favicon.ico', // Optional: Shortcut icon
+    apple: 'apple-touch-icon.png', // Optional: Apple touch icon
     other: {
       rel: 'icon',
       url: '/favicon-32x32.png', // Optional: Additional icon formats
